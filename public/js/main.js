44fd060ab41ad0aa1233a316ed58171a94c28af4
Todd Wolfson <todd@twolfson.com>
2014-11-08 15:48:32 -0800

    Added renderer for first badge

--- public/js/main.js
+++ public/js/main.js
@@ -7,18 +7,35 @@ var categories = require('./categories.json');
 var explorer = new BadgeExplorer(document.body);
 
 // TODO: If there is no badge selected
-if (true) {
-    // DEV: We could query the DOM but that would be sloooow
-    // Find the first badge and render it
+// DEV: Use a closure to prevent conflict with `hashchange` listener
+(function renderFirstBadge () {
+    if (true) {
+        // DEV: We could query the DOM but that would be sloooow
+        // Find the first badge and render it
+        var badge;
+        var i = 0;
+        var len = badges.length;
+        for (; i < len; i++) {
+            badge = badges[i];
+            if (badge.badgeCategory === categories[0].category) {
+                break;
+            }
+        }
+        explorer.render(badge);
+    }
+}());
+
+// When the URL hash changes
+// http://caniuse.com/#search=hashchange
+window.addEventListener('hashchange', function () {
+    // Find a matching badge
     var i = 0;
     var len = badges.length;
+    var slug = window.hash.slice(1);
     for (; i < len; i++) {
         var badge = badges[i];
         if (badge.badgeCategory === categories[0].category) {
             break;
         }
     }
-    explorer.render(badge);
-}
-
-// When the URL hash changes
+});
