37e541a92f40d03ab7846ecd13e208a8fe93658d
Todd Wolfson <todd@twolfson.com>
2014-11-08 15:36:18 -0800

    Moving from hardcoded badge info to dynamic badge info

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,11 @@
 // Load in dependencies
 var BadgeExplorer = require('./badge-explorer');
-
-// TODO: Instead of hardcoding this info, fetch it from the API at
-// https://www.khanacademy.org/api/v1/badges?casing=camel and
-// https://www.khanacademy.org/api/v1/badges/categories?casing=camel
-// TODO: Make this page not always render the same badge
-var virusBadge = {
-    'description': 'Virus',
-    'icons': {
-        'small': 'https://www.khanacademy.org/images/badges/meteorite/virus-40x40.png',
-        'compact': 'https://www.khanacademy.org/images/badges/meteorite/virus-60x60.png',
-        'large': 'https://www.khanacademy.org/images/badges/meteorite/virus-512x512.png',
-        'email': 'https://www.khanacademy.org/images/badges/meteorite/virus-70x70.png'
-    },
-    'absoluteUrl': 'https://www.khanacademy.org/badges/virus',
-    'badgeCategory': 0,
-    'points': 0,
-    'safeExtendedDescription': 'Share a program you\'ve created via Facebook, Twitter, or email',
-    'slug': 'virus'
-};
+var badges = require('./badges.json');
 
 // Initialize our badge explorer against the body and render the virus badge
 var explorer = new BadgeExplorer(document.body);
-explorer.render(virusBadge);
+
+// TODO: If there is no badge selected, display the first one
+explorer.render(badges[0]);
+
+// When the URL hash changes
