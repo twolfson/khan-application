45a60169ad2e422925b26696d37529940c077845
Todd Wolfson <todd@twolfson.com>
2014-11-08 15:53:43 -0800

    Completed rewrite to use URL badges

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,40 @@ var categories = require('./categories.json');
 // Initialize our badge explorer against the body and render the virus badge
 var explorer = new BadgeExplorer(document.body);
 
+// Define helper to render a badge for a slug
+function renderCurrentBadge() {
+    // Find a matching badge for the hash (e.g. `#fact-checker` -> `fact-checker`)
+    // TODO: Consider using a map for faster lookups `O(1)` (e.g. `badges['fact-checker']`)
+    var matchingBadge = null;
+    var i = 0;
+    var len = badges.length;
+    var slug = window.hash.slice(1);
+    for (; i < len; i++) {
+        // If the badge's slug matches our slug, save it and stop
+        var badge = badges[i];
+        if (badge.slug === slug) {
+            matchingBadge = badge;
+            break;
+        }
+    }
+
+    // If we have a match, update the explorer
+    if (matchingBadge) {
+        explorer.render(matchingBadge);
+    }
+
+    // Return the result
+    return matchingBadge;
+}
+
 // TODO: If there is no badge selected
 // DEV: Use a closure to prevent conflict with `hashchange` listener
 (function renderFirstBadge () {
-    if (true) {
+    // Use the current badge from the URL
+    var _badge = renderCurrentBadge();
+
+    // If there is none
+    if (_badge === null) {
         // DEV: We could query the DOM but that would be sloooow
         // Find the first badge and render it
         var badge;
@@ -25,17 +55,6 @@ var explorer = new BadgeExplorer(document.body);
     }
 }());
 
-// When the URL hash changes
+// When the URL hash changes, update the badge to the one from the hash
 // http://caniuse.com/#search=hashchange
-window.addEventListener('hashchange', function () {
-    // Find a matching badge
-    var i = 0;
-    var len = badges.length;
-    var slug = window.hash.slice(1);
-    for (; i < len; i++) {
-        var badge = badges[i];
-        if (badge.badgeCategory === categories[0].category) {
-            break;
-        }
-    }
-});
+window.addEventListener('hashchange', renderCurrentBadge);
