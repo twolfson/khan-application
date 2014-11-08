90427ef7b526571d72fd0621e3cbd01923b58951
Todd Wolfson <todd@twolfson.com>
2014-11-08 15:40:50 -0800

    Added rendering for default badge

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,24 @@
 // Load in dependencies
 var BadgeExplorer = require('./badge-explorer');
 var badges = require('./badges.json');
+var categories = require('./categories.json');
 
 // Initialize our badge explorer against the body and render the virus badge
 var explorer = new BadgeExplorer(document.body);
 
-// TODO: If there is no badge selected, display the first one
-explorer.render(badges[0]);
+// TODO: If there is no badge selected
+if (true) {
+    // DEV: We could query the DOM but that would be sloooow
+    // Find the first badge and render it
+    var i = 0;
+    var len = badges.length;
+    for (; i < len; i++) {
+        var badge = badges[i];
+        if (badge.badgeCategory === categories[0].category) {
+            break;
+        }
+    }
+    explorer.render(badge);
+}
 
 // When the URL hash changes
