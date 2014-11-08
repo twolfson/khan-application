793a497d3d83e371e8e2979579d1f7a39bbf88db
Todd Wolfson <todd@twolfson.com>
2014-11-08 13:32:19 -0800

    Added basic assertion

--- release.sh
+++ release.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-# Stop at first error and echo commands
+# Stop at first error
 set -e
-set -x
 
 # Grab the semver
 semver="$1"
-echo "$semver"
+if test "$semver" = ""; then
+  echo "Semver was not provided to \`release.sh\`. Please provide one" 1>&2
+  echo "./release.sh <semver>" 1>&2
+  exit 1
+fi
+
+# Echo commands
+set -x
 
 # DEV: We could use `npm-run-script` since as of 2.0, it supports variables
 #   However, I am more familiar with this at the moment
