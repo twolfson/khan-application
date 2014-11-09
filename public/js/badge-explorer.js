17eee17791631074fd31983f31ae1c78aaf40b6d
Todd Wolfson <todd@twolfson.com>
2014-11-08 16:07:33 -0800

    Moved to memoized references

--- public/js/badge-explorer.js
+++ public/js/badge-explorer.js
@@ -5,15 +5,16 @@ function BadgeExplorer(container) {
     this.$switcher = $(container);
 
     // Generate our content, append them together, and add to our container
-    this.$badge = $(
-        '<div class="full-badge clearfix">' +
-            '<div class="full-badge-icon">' +
-                '<img>' +
-            '</div>' +
-            '<div class="full-badge-title"></div>' +
-            '<div class="full-badge-description"></div>' +
-        '</div>'
-    );
+    // DEV: Create and memoize references to items once to prevent requerying
+    this.$badge = $('<div class="full-badge clearfix">');
+        this.$imgContainer = $('<div class="full-badge-icon">');
+            this.$img = $('<img>');
+            this.$imgContainer.append(this.$img);
+        this.$badge.append(this.$imgContainer);
+        this.$title = $('<div class="full-badge-title"></div>');
+        this.$badge.append(this.$title);
+        this.$description = $('<div class="full-badge-description"></div>');
+        this.$badge.append(this.$description);
     this.$switcher.append(this.$badge);
 }
 BadgeExplorer.prototype = {
@@ -22,10 +23,10 @@ BadgeExplorer.prototype = {
         // DEV: Since the image could take a bit of time to load, set it to nothing (otherwise, we will have lag between content pieces)
         // DEV: We don't want to use a spritesheet because that would be *really* big and defeat the purpose
         // TODO: We could lazy load large images from the current set but that is killing bandwidth
-        this.$badge.find('.full-badge-icon img').attr('src', null);
-        this.$badge.find('.full-badge-icon img').attr('src', badge.icons.large);
-        this.$badge.find('.full-badge-title').text(badge.description);
-        this.$badge.find('.full-badge-description').text(badge.safeExtendedDescription);
+        // TODO: Upload images to CDN
+        this.$img.attr('src', null).attr('src', badge.icons.large);
+        this.$title.text(badge.description);
+        this.$description.text(badge.safeExtendedDescription);
     }
 };
 
