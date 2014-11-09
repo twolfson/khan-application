e95d341a6ac43fda84193aa6cc7edeb6e864cdf0
Todd Wolfson <todd@twolfson.com>
2014-11-08 16:32:40 -0800

    Added functionality for previous and next arrows

--- public/js/main.js
+++ public/js/main.js
@@ -5,20 +5,28 @@ var categories = require('./categories.json');
 
 // Initialize our badge explorer against the body and render the virus badge
 var explorer = new BadgeExplorer($('.badge-switcher'));
+var $prevBadge = $('.badge-switch-left');
+var $nextBadge = $('.badge-switch-right');
 
-// Define a helper to keep track of the current badge
-// DEV: This is used for the previous and next function
-var currentBadge;
-function renderBadge(badge) {
-    currentBadge = badge;
+// When we are rendering a new badge
+function renderBadge(index) {
+    // Update the displayed badge
+    var badge = badges[index];
     explorer.render(badge);
+
+    // Update the arrows to match previous and next
+    // DEV: Since we have stable sorted we can directly access the previous/next
+    var prevBadge = badges[index - 1] || badges[badges.length - 1];
+    var nextBadge = badges[index + 1] || badges[0];
+    $prevBadge.attr('href', '#' + prevBadge.slug);
+    $nextBadge.attr('href', '#' + nextBadge.slug);
 }
 
 // Define helper to render a badge for a slug
 function renderCurrentBadge() {
     // Find a matching badge for the hash (e.g. `#fact-checker` -> `fact-checker`)
     // TODO: Consider using a map for faster lookups `O(1)` (e.g. `badges['fact-checker']`)
-    var matchingBadge = null;
+    var matchingBadgeIndex = null;
     var i = 0;
     var len = badges.length;
     var slug = window.location.hash.slice(1);
@@ -26,18 +34,18 @@ function renderCurrentBadge() {
         // If the badge's slug matches our slug, save it and stop
         var badge = badges[i];
         if (badge.slug === slug) {
-            matchingBadge = badge;
+            matchingBadgeIndex = i;
             break;
         }
     }
 
     // If we have a match, update the explorer
-    if (matchingBadge) {
-        renderBadge(matchingBadge);
+    if (matchingBadgeIndex !== null) {
+        renderBadge(i);
     }
 
     // Return the result
-    return matchingBadge;
+    return matchingBadgeIndex;
 }
 
 // DEV: Use a closure to prevent conflict with `hashchange` listener
@@ -48,7 +56,7 @@ function renderCurrentBadge() {
     // If there is none, render the first badge
     // DEV: We have stable sorted the badges into categories for easy lookups like this
     if (_badge === null) {
-        renderBadge(badges[0]);
+        renderBadge(0);
     }
 }());
 
